6661134f28c27606ec998bf578777c39052aebec
Lodkahs <omalojiu@gmail.com>
2023-03-17 20:50:16 +0200

    upload image on firebase

--- SnapchatClone/ViewControllers/UploadVC.swift
+++ SnapchatClone/ViewControllers/UploadVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -22,7 +23,51 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     @IBAction func uploadClickedButton(_ sender: Any) {
         
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
         
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                    
+                } else {
+                    
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            
+                            let imageURL = url?.absoluteString
+                            
+                            let firestore = Firestore.firestore()
+                            
+                            let snapDictionary = ["imageURL" : imageURL!, "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                            
+                            firestore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                                
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                } else {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "select")
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
         
     }
     
@@ -38,6 +83,13 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         uploadImageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 
